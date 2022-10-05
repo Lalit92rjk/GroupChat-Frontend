74b8759890aaec41847b72e0509d91e02ee2ccc2
Lalit Rajak <lalit92rajak@gmail.com>
2022-10-06 04:07:48 +0530

    getting message

--- chat/chat.js
+++ chat/chat.js
@@ -41,6 +41,41 @@ sendmsg.addEventListener('click', (e) => {
 });
 
 
+  axios.get("http://localhost:3000/getmessages", {
+      headers: { authorization: token },
+    })
+    .then((result) => {
+      let res = "";
+
+      for (let i = 0; i < result.data.result.length; i++) {
+        console.log(result.data.result[i].username == nam);
+        if(result.data.result[i].username==nam){
+          res += `
+            <div class="p-2 indimsg " style="background:black;border-bottom:1px dotted snow; color:snow;border-radius:7px; ">
+              <span style="margin-left:30%;" >you : 
+              </span>
+              <span>${result.data.result[i].msg}</span>
+            </div>
+          `;
+        }
+        else{
+          res += `
+            <div class="p-2 indimsg " style="background:rgb(8, 87,87); color:snow; border-bottom:1px solid white; border-radius:6px; ">
+              <span  >${result.data.result[i].username} : 
+              </span>
+              <span>${result.data.result[i].msg}</span>
+            </div>
+          `;
+        }
+      }
+        allmsgs.innerHTML = res;
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+
+
+
 signout.addEventListener('click', () => {
 
   localStorage.clear();
